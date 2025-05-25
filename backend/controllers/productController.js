374e6b0cf54644abfe7141bcff0f7492e065ccc7
ssushilgit <shresthasushil741@gmail.com>
2025-05-25 18:31:13 +0545

    Controllers for update and delete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -75,6 +75,66 @@ export const getSingleProduct = async (req, res) => {
 };
 
 // update product
-export const updateProuduct = async (req, res) => {};
+export const updateProuduct = async (req, res) => {
+    const {id} = req.params;
+    const {name, price, image} = req.body;
 
-export const deleteProduct = async (req, res) => {};
+    try {
+        const updateProduct = await sql `
+            UPDATE products
+            SET name=${name}, price= ${price}, image=${image}
+            WHERE id = ${id}
+            RETURNING *
+        `
+
+        if(updateProduct === 0){
+            return res.status(404).json({
+                success: false,
+                message : "Product not found"
+            })
+        }
+
+        res.status(200).json({
+            success : true,
+            data : updateProduct[0]
+        })
+        
+    } catch (error) {
+        console.log("Error in updateProduct function ",errror)
+        res.status(500).json({
+            success : false,
+            message : "Internal Server Error"
+        })
+        
+    }
+};
+
+// delete product
+export const deleteProduct = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const deletedProduct = await sql`
+            DELTE FROM products WHERE id=${id}
+            RETURNING *
+        `
+        if(deletedProduct === 0){
+            res.status(404).json({
+                success : false,
+                message : "Product not found"
+            })
+        }
+
+        res.status(200).json({
+            success : true,
+            data : deletedProduct[0]
+        })
+
+    } catch (error) {
+        console.log("Error in deleteProduct function", error)
+        res.status(500).json({
+            success : false,
+            message : "Error in Interval Server"
+        })
+    }
+
+};
